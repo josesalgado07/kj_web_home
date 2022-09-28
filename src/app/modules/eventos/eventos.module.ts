8fa3d652b4cfaa4214eca1df55e24f2e0411e8d1
Jose Salgado <jose.salgado1501@gmail.com>
2022-09-28 18:13:02 -0500

    se ajusta tabla general

--- src/app/modules/eventos/eventos.module.ts
+++ src/app/modules/eventos/eventos.module.ts
@@ -5,12 +5,12 @@ import { EventosRoutingModule } from './eventos-routing.module';
 import { EventosPageComponent } from './pages/eventos-page/eventos-page.component';
 import { EventosAddPageComponent } from './pages/eventos-add-page/eventos-add-page.component';
 import { FormsModule } from '@angular/forms';
-import { InputTextComponent } from 'src/app/shared/input-text/input-text.component';
 import { CalendarComponent } from 'src/app/shared/calendar/calendar.component';
-import { TableFilterComponent } from 'src/app/shared/table-filter/table-filter.component';
 
 import { CardModule } from 'primeng/card';
 import { SharedModule } from '@shared/shared.module';
+
+import { CalendarModule } from 'primeng/calendar';
 
 @NgModule({
   declarations: [
@@ -25,7 +25,8 @@ import { SharedModule } from '@shared/shared.module';
 
     //standalone components
     CalendarComponent,
-    SharedModule
+    SharedModule,
+    CalendarModule
   ]
 })
 export class EventosModule { }
